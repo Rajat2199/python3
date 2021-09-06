ef525e429477bf30c2781eb1380502d5665666e8
Rajat2199 <rajatsharma2199@gmail.com>
2021-09-06 22:40:25 +0530

    previous again

--- max.py
+++ max.py
@@ -1,12 +1,13 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 a=20 , b= 20
 =======
 >>>>>>> parent of 254fed0... modified file
+=======
+a=10 , b= 20
+>>>>>>> parent of ac4d15b... Changes made
 def maximum(a,b):
-    if a>b:
+    if a>=b:
         return a
-    elif b>a:
-        return b
     else:
-        print("Both are equal") 
-
+        return b 

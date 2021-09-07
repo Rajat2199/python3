37e43cf88973c4f2abe14924a239839c36720293
Rajat2199 <rajatsharma2199@gmail.com>
2021-09-07 10:48:29 +0530

    Changes made

--- max.py
+++ max.py
@@ -1,10 +1,16 @@
+<<<<<<< HEAD
 #this program finds larger of the two given numbers
 a=10
 b=20
+=======
+a=10 , b= 20
+>>>>>>> parent of ac4d15b... Changes made
 def maximum(a,b):
-    if a>b:
+    if a>=b:
         return a
-    elif b>a:
-        return b
     else:
+<<<<<<< HEAD
         print("Both numbers are equal")
+=======
+        return b 
+>>>>>>> parent of ac4d15b... Changes made
